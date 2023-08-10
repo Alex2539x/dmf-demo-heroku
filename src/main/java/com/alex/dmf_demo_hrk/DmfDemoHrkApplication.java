a67ac5a72de18eb5ea376f53aec26c5b6b9bda0b
Alex Ho <alex2539x@gmail.com>
2023-08-09 23:58:56 -0400

    spring boot thymeleaf example

---
+++ src/main/java/com/alex/dmf_demo_hrk/DmfDemoHrkApplication.java
@@ -0,0 +1,13 @@
+package com.alex.dmf_demo_hrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmfDemoHrkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmfDemoHrkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alex/dmf_demo_hrk/DmfDemoHrkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.dmf_demo_hrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmfDemoHrkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
